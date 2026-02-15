2f61ee36937dec1f0ad7bb909760f3ad91637778
Dragos Varovici <dvarovici.work@gmail.com>
2026-02-15 21:20:10 +0200

    DevContainer

--- Sources/DockerClientSwift/Helper/HTTPClient+ExecuteOnSocket.swift
+++ Sources/DockerClientSwift/Helper/HTTPClient+ExecuteOnSocket.swift
@@ -1,8 +1,8 @@
+import AsyncHTTPClient
 import Foundation
+import Logging
 import NIO
 import NIOHTTP1
-import AsyncHTTPClient
-import Logging
 
 extension HTTPClient {
     /// Executes a HTTP request on a socket.
@@ -15,15 +15,14 @@ extension HTTPClient {
     ///   - logger: The logger to use for this request.
     ///   - headers: Custom HTTP headers.
     /// - Returns: Returns an `EventLoopFuture` with the `Response` of the request
-    public func execute(_ method: HTTPMethod = .GET, socketPath: String, urlPath: String, body: Body? = nil, deadline: NIODeadline? = nil, logger: Logger, headers: HTTPHeaders) -> EventLoopFuture<Response> {
-        do {
-            guard let url = URL(httpURLWithSocketPath: socketPath, uri: urlPath) else {
-                throw HTTPClientError.invalidURL
-            }
-            let request = try Request(url: url, method: method, headers: headers, body: body)
-            return self.execute(request: request, deadline: deadline, logger: logger)
-        } catch {
-            return self.eventLoopGroup.next().makeFailedFuture(error)
+    public func execute(
+        _ method: HTTPMethod = .GET, socketPath: String, urlPath: String, body: Body? = nil,
+        deadline: NIODeadline? = nil, logger: Logger, headers: HTTPHeaders
+    ) async throws -> Response {
+        guard let url = URL(httpURLWithSocketPath: socketPath, uri: urlPath) else {
+            throw HTTPClientError.invalidURL
         }
+        let request = try Request(url: url, method: method, headers: headers, body: body)
+        return try await self.execute(request: request, deadline: deadline, logger: logger)
     }
 }
