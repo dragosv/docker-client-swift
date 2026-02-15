c8d6260180468cd7266f5398d34887e87a9e7dd9
Dragos Varovici <dvarovici.work@gmail.com>
2026-02-15 21:20:03 +0200

    Package updates

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "docker-client-swift",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v12)],
     products: [
-        .library(name: "DockerClientSwift", targets: ["DockerClientSwift"]),
+        .library(name: "DockerClientSwift", targets: ["DockerClientSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.18.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.94.1"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.31.0"),
     ],
     targets: [
         .target(
